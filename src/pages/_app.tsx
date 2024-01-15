8e0a88742d5fe52c516b81896cd0b923714500fe
Ben Haynes <ben.haynes@ridgeway.com>
2024-01-15 16:18:38 +0000

    remaining-code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '~/styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { AppProps } from 'next/app'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function App ({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <div className={inter.className}>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+    </div>
+  )
 }
