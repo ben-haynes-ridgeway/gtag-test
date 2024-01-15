c4c866f9dd4591a9bae5591aa67cd8e1e710e0e7
Ben Haynes <ben.haynes@ridgeway.com>
2024-01-15 16:33:04 +0000

    add gtag to page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,18 @@ export default function Document (): JSX.Element {
   return (
     <Html lang="en">
       <Head />
+
       <body>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-X3651L5J2W" />
+        <script dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-X3651L5J2W');
+            `
+        }}
+        />
         <Main />
         <NextScript />
       </body>
