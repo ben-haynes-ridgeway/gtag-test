4c77b28f02f1f9a20b723c177173c59152bbf49f
Ben Haynes <ben.haynes@ridgeway.com>
2024-01-16 10:16:02 +0000

    update gtag code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,25 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document (): JSX.Element {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WNJJD89J');`
+        }}
+        />
+      </Head>
 
       <body>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-X3651L5J2W" />
-        <script dangerouslySetInnerHTML={{
+
+        <noscript dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WNJJD89J"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+        }}
+        />
+        {/* <script dangerouslySetInnerHTML={{
           __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
@@ -15,7 +29,7 @@ export default function Document (): JSX.Element {
               gtag('config', 'G-X3651L5J2W');
             `
         }}
-        />
+        /> */}
         <Main />
         <NextScript />
       </body>
